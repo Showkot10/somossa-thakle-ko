f1b6dc2d3f2160bd2618b7f961578e901dcb3036
Showkot10 <98248401+Showkot10@users.noreply.github.com>
2022-02-17 11:40:42 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    //jquery for toggle sub manus   
+    $('.sub-btn').click(function(){
+     $(this).next('.sub-manu').slideToggle();
+     $(this).find('.dropdown').toggleClass('rotate');
+     });
+     $('.second-sub-manu').click(function(){
+     $(this).find('.dropdown').toggleClass('rotate');
+     });
+   
+   
+     //jquery for and collapse the sidebar
+     $('.manu-btn').click(function(){
+         $('.side-bar').addClass('active');
+        $('.manu-btn').css("visibility","hidden");
+        $('#search-icon').css("visibility","hidden");
+        $('#search-closer').css("visibility","hidden");
+        $('.search-form').css("visibility","hidden");
+        $('#featured').css("visibility","hidden");
+     });
+     $('.close-btn').click(function(){
+         $('.side-bar').removeClass('active');
+         $('.manu-btn').css("visibility","visible");
+         $('#search-icon').css("visibility","visible");
+         $('.search-form').css("visibility","visible");
+         $('#featured').css("visibility","visible");
+     });
+     //search icons
+     $('#search-icon').click(function(){
+         $('.search-form').addClass('active');
+        $('#search-icon').css("visibility","hidden"); 
+        $('#search-closer').css("visibility","visible"); 
+       
+     });
+     $('#search-closer').click(function(){
+         $('.search-form').removeClass('active');
+        $('#search-closer').css("visibility","hidden"); 
+        $('#search-icon').css("visibility","visible"); 
+        
+     });
+   });
+
+   window.addEventListener('DOMContentLoaded', function() {
+    $(function() {
+        var clipboard = new ClipboardJS('.copybutton');   
+         $(".copybutton").click(function() { 
+                 $(this).html('Copied'); 
+                  var that = $(this);
+                  var intrvrl =  setInterval(function(){  
+                  that.html('Copy'); 
+                  clearInterval(intrvrl); }, 
+                10000);
+         });
+    });
+        });
+
+     
